5f35eef99a92dee6844bf6f830716c1d8020a60d
ThunderBolt-OS <soham.bhoir@somaiya.edu>
2022-06-12 22:59:07 +0530

    feat: navbar (not responsive)

--- client/components/Navbar/index.js
+++ client/components/Navbar/index.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import styles from './styles.module.scss'
+import { FaBars } from 'react-icons/fa'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className={styles.container}>
+      
+      <div className={styles.navbar}>
+
+        <div className={styles.title}>
+          <div className={styles.logo}>ThunderBolt</div>          
+        </div> 
+
+        <div className={styles.navlinks}>
+          <ul className={styles.ul}>
+            <li className={styles.li}> <span>1.0</span> About Me</li>
+            <li className={styles.li}> <span>2.0</span> Experience</li>
+            <li className={styles.li}> <span>3.0</span> Projects</li>
+            <li className={styles.li}> <span>4.0</span> Testimonials</li>
+            <li className={styles.li}> <span>5.0</span> Contact Me</li>
+          </ul>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
